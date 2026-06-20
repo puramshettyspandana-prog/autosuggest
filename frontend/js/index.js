e047ff1bed1802f44c59a09d32150547eb699870
spandana <puramshettyspandana@gmail.com>
2026-06-20 09:13:11 +0530

    switch user

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,22 @@
-//alert( "Hello from JS !" );
-console.log( "Hello from JS!" );
+//alert( "JS is loading" );
+//console.log( "Hello from JS!" );
+let isMale = true;
+
+function toggleCard()
+{
+    const img = document.querySelector( "img" );
+    const name = document.querySelector( "h2" );
+    const gender = document.querySelector( "p" );
+
+    if ( isMale ) {
+        img.src = "../image/woman.png"; // female image
+        name.textContent = "Spandana";
+        gender.textContent = "Female";
+    } else {
+        img.src = "../image/john.png";// male image
+        name.textContent = "John";
+        gender.textContent = "Male";
+    }
+
+    isMale = !isMale;
+}
